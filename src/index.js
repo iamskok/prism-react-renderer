b5209aaaad7366905d9c33733e50518b225abbcf
Vladimir <vladimir.skok@bvaccel.com>
2020-04-23 18:02:43 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// @flow
+
+import Prism from './vendor/prism'
+import defaultProps from './defaultProps'
+import Highlight from './components/Highlight'
+
+export {
+  Prism,
+  defaultProps
+}
+
+export default Highlight
---
+++ src/vendor/prism/includeLangs.js
@@ -0,0 +1,22 @@
+// @flow
+
+// These are the languages that'll be included in the generated
+// prism/index.js file
+
+module.exports = {
+  markup: true,
+  bash: true,
+  css: true,
+  "css-extras": true,
+  javascript: true,
+  jsx: true,
+  "js-extras": true,
+  graphql: true,
+  json: true,
+  markdown: true,
+  scss: true,
+  toml: true,
+  tsx: true,
+  typescript: true,
+  yaml: true
+};
